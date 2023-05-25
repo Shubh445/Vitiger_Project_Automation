994826a841802d0bec66ae61bdf6735bbca89d2a
Shubham  Ramteke <126812253+Shubh445@users.noreply.github.com>
2023-05-25 11:01:02 +0530

    Add files via upload

---
+++ emptyWindow.java
@@ -0,0 +1,13 @@
+package Basic;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class emptyWindow {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver=new ChromeDriver(); // manage the Window();
+		driver.get(" ");
+		
+	}
+}
